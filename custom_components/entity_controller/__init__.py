8c972be58ef8a7f35c908cfbe6361ffbee033891
dennis84de <github@djaeschke.de>
2020-12-10 21:39:34 +0100

    Update entity controller for lagging flux_led

--- custom_components/entity_controller/__init__.py
+++ custom_components/entity_controller/__init__.py
@@ -95,7 +95,7 @@ from .const import (
     CONF_IGNORED_EVENT_SOURCES,
     CONSTRAIN_START,
     CONSTRAIN_END,
-    
+    CONF_IGNORE_STATE_CHANGES_UNTIL,
     CONTEXT_ID_CHARACTER_LIMIT
 )
 
@@ -145,7 +145,7 @@ ENTITY_SCHEMA = vol.Schema(
         vol.Optional(CONF_TRIGGER_ON_DEACTIVATE, default=None): cv.entity_ids,
         vol.Optional(CONF_STATE_ENTITIES, default=[]): cv.entity_ids,
         vol.Optional(CONF_BLOCK_TIMEOUT, default=None): cv.positive_int,
-        # vol.Optional(CONF_IGNORE_STATE_CHANGES_UNTIL, default=None): cv.positive_int,
+        vol.Optional(CONF_IGNORE_STATE_CHANGES_UNTIL, default=None): cv.positive_int,
         vol.Optional(CONF_NIGHT_MODE, default=None): MODE_SCHEMA,
         vol.Optional(CONF_STATE_ATTRIBUTES_IGNORE, default=[]): cv.ensure_list,
         vol.Optional(CONF_IGNORED_EVENT_SOURCES, default=[]): cv.ensure_list,
@@ -639,8 +639,11 @@ class Model:
             )
         self.set_context(new.context)
         if self.is_active_timer():
-            self.log.debug("state_entity_state_change :: We are in active timer and the state of observed state entities changed.")
-            self.control()
+            if self.is_within_grace_period(): # check if we are within the grace period after making a service call (this avoids EC blocking itself)
+                self.log.debug("state_entity_state_change :: This state change is within %i seconds of calling a service. Ignoring this state change because its probably caused by EC itself." % self.config.get(CONF_IGNORE_STATE_CHANGES_UNTIL, 2))
+            else:
+                self.log.debug("state_entity_state_change :: We are in active timer and the state of observed state entities changed.")
+                self.control()
 
         if self.is_blocked() or self.is_active_stay_on(): # if statement required to avoid MachineErrors, cleaner than adding transitions to all possible states.
             self.enable()
@@ -723,10 +726,10 @@ class Model:
     def is_override_state_off(self):
         return self._override_entity_state() is None
 
-    # def is_within_grace_period(self):
-    #     """ Dtermines if the last service call EC made was within the last 2 seconds. 
-    #     This is important or else EC will react to state changes caused by EC itself which results in going into blocked state."""
-    #     return datetime.now() < self.ignore_state_changes_until
+    def is_within_grace_period(self):
+        """ Dtermines if the last service call EC made was within the last 2 seconds. 
+        This is important or else EC will react to state changes caused by EC itself which results in going into blocked state."""
+        return datetime.now() < self.ignore_state_changes_until
 
     def is_override_state_on(self):
         return self._override_entity_state() is not None
@@ -1471,8 +1474,8 @@ class Model:
     def call_service(self, entity, service, **service_data):
         """ Helper for calling HA services with the correct parameters """
         self.log.debug("call_service :: Calling service " + service + " on " + entity)
-        # self.ignore_state_changes_until = datetime.now() + timedelta(seconds=self.config.get(CONF_IGNORE_STATE_CHANGES_UNTIL, 2))
-        # self.log.debug("call_service :: Setting ignore_state_changes_until to " + str(self.ignore_state_changes_until))
+        self.ignore_state_changes_until = datetime.now() + timedelta(seconds=self.config.get(CONF_IGNORE_STATE_CHANGES_UNTIL, 2))
+        self.log.debug("call_service :: Setting ignore_state_changes_until to " + str(self.ignore_state_changes_until))
 
         domain, e = entity.split(".")
         if service in ['turn_on','turn_off'] and domain in self.homeassistant_turn_on_domains:
