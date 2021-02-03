4bf2a9dd082c4a2a2690603dc47bddc206c6d2b2
dennis84de <github@djaeschke.de>
2021-02-03 16:03:06 +0100

    Major changes after migration to Supervisor

--- bash_scripts/upgrade-hass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo su -s /bin/bash homeassistant <<'EOF'
-source /srv/homeassistant/bin/activate
-pip3 install --upgrade homeassistant
-exit
-EOF
-
-sudo systemctl restart home-assistant@homeassistant.service
