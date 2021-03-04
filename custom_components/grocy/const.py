4450942429bbd4a8fa063c1b683a793ee2b7e461
dennis84de <github@djaeschke.de>
2021-03-04 16:43:51 +0100

    Update custom components

--- custom_components/grocy/const.py
+++ custom_components/grocy/const.py
@@ -4,7 +4,7 @@ from enum import Enum
 # Base component constants
 NAME = "Grocy"
 DOMAIN = "grocy"
-VERSION = "v2.2.2"
+VERSION = "v3.0.1"
 
 ISSUE_URL = "https://github.com/custom-components/grocy/issues"
 
