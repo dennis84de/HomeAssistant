9d5cd7139d172e379b0ef7e2a051aaff35b31cab
dennis84de <github@djaeschke.de>
2023-01-02 16:11:00 +0100

    Update plugins

--- www/community/lovelace-slider-entity-row/rollup.config.js
+++ www/community/lovelace-slider-entity-row/rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import typescript from "rollup-plugin-typescript2";
-import terser from "@rollup/plugin-terser";
+import { terser } from "rollup-plugin-terser";
 import babel from "@rollup/plugin-babel";
 
 const dev = process.env.ROLLUP_WATCH;
@@ -9,7 +9,7 @@ const dev = process.env.ROLLUP_WATCH;
 export default {
   input: "src/main.ts",
   output: {
-    file: "auto-entities.js",
+    file: "slider-entity-row.js",
     format: "es",
   },
   plugins: [
